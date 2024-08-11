005e2e22c718b5d83cb186d140a1bbc7f9d633d0
Saadain Rehman <saadhabib699@gmail.com>
2024-08-11 14:15:18 +0500

    [ADDED]Added a function to show the converted currency

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ const BASE_URL =
   "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/v1/{endpoint}";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
 
 for (let select of dropdowns) {
   for (currCode in countryList) {
@@ -36,7 +40,42 @@ for (let select of dropdowns) {
 const updateFlag = (element) => {
     let currCode = element.value;
     let countryCode = countryList[currCode];
-    let newSrc = `"https://flagsapi.com/${ currCode}/flat/64.png"`
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
     let img = element.parentElement.querySelector("img");
     img.src = newSrc;
 }
+
+// btn.addEventListener("click" , (evt) => {
+//   evt.preventDefault();
+//   let amount = document.querySelector(".amount input");
+//   let amtVal = amount.value;
+//   if (amtVal === "" || amtVal < 1){
+//     amtVal = 1 ;
+//     amount.val = "1";
+//   }
+  // console.log(amtVal)
+  // console.log( fromCurr.value.toLowerCase() , toCurr.value.toLowerCase());
+//   const fromCurr = fromCurr.toLowerCase();
+//   const toCurr = toCurr.toLowerCase();
+//   const URL = `${BASE_URL}/currencies/${fromCurrency}.json`;
+// })
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+
+  const URL = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data[toCurr.value.toLowerCase()];
+
+  let finalAmount = amtVal * rate;
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
+
+
